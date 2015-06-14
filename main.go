170a280f1f585fa84cdc11f59ad94fc969c3f4f1
Bernhard Biskup <bbiskup@gmx.de>
2015-06-14 12:12:39 +0200

    Use gorilla mux instead of go stdlib ServeMux

--- main.go
+++ main.go
@@ -3,24 +3,24 @@ package main
 import (
 	"github.com/bbiskup/edify-web/defs"
 	"github.com/bbiskup/edify-web/handlers"
-
-	//"github.com/gorilla/mux"
+	"github.com/gorilla/mux"
 	"net/http"
 )
 
 func main() {
-	mux := http.NewServeMux()
+	r := mux.NewRouter()
 	static := http.FileServer(http.Dir(defs.STATIC_DIR))
 	bower := http.FileServer(http.Dir(defs.BOWER_DIR))
 
-	mux.Handle("/static/edify/", http.StripPrefix("/static/edify/", static))
-	mux.Handle("/static/bower/", http.StripPrefix("/static/bower/", bower))
-	mux.HandleFunc("/", handlers.Index)
-	mux.HandleFunc("/about/", handlers.About)
+	r.HandleFunc("/", handlers.Index)
+	r.HandleFunc("/about/", handlers.About)
+
+	r.PathPrefix("/static/edify/").Handler(http.StripPrefix("/static/edify/", static))
+	r.PathPrefix("/static/bower/").Handler(http.StripPrefix("/static/bower/", bower))
 
 	server := &http.Server{
 		Addr:    "0.0.0.0:8001",
-		Handler: mux,
+		Handler: r,
 	}
 	if err := server.ListenAndServe(); err != nil {
 		panic(err)
