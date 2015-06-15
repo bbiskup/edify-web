85639ba40acea3174ade897d03d496683c9b3d75
Bernhard Biskup <bbiskup@gmx.de>
2015-06-15 13:02:48 +0200

    Benchmarks for search with zero/all results

--- handlers/specsearch_test.go
+++ handlers/specsearch_test.go
@@ -2,22 +2,46 @@ package handlers
 
 import (
 	"github.com/stretchr/testify/assert"
+	//	"log"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
-func TestSpecSearchNoResults(t *testing.T) {
+/*func TestSearchMsgSpecsNoResults(t *testing.T) {
 	req, err := http.NewRequest("GET", "", nil)
 	assert.Nil(t, err)
 	w := httptest.NewRecorder()
 	handler := http.HandlerFunc(SpecSearch)
 	handler.ServeHTTP(w, req)
 	assert.Equal(t, http.StatusOK, w.Code)
+}*/
+
+func TestSpecSearchAllResults(t *testing.T) {
+	req, err := http.NewRequest("GET", "", nil)
+	assert.Nil(t, err)
+	w := httptest.NewRecorder()
+	handler := http.HandlerFunc(SpecSearch)
+	handler.ServeHTTP(w, req)
+	assert.Equal(t, http.StatusOK, w.Code)
+	//log.Printf("Body: %s", w.Body)
+}
+
+func BenchmarkSpecSearchAllResults(b *testing.B) {
+	req, err := http.NewRequest("GET", "", nil)
+	assert.Nil(b, err)
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		w := httptest.NewRecorder()
+		handler := http.HandlerFunc(SpecSearch)
+		handler.ServeHTTP(w, req)
+		assert.Equal(b, http.StatusOK, w.Code)
+	}
 }
 
 func BenchmarkSpecSearchNoResults(b *testing.B) {
-	req, err := http.NewRequest("GET", "", nil)
+	req, err := http.NewRequest("GET", "?searchterm=sdfusfuspdfsdfjsfjsldfjopsfoa", nil)
 	assert.Nil(b, err)
 
 	b.ResetTimer()
