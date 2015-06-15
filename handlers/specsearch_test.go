a5091aa276d910a96f7a1305e6d1dcd28708e1bf
Bernhard Biskup <bbiskup@gmx.de>
2015-06-15 13:35:23 +0200

    Added test cases

--- handlers/specsearch_test.go
+++ handlers/specsearch_test.go
@@ -8,14 +8,50 @@ import (
 	"testing"
 )
 
-/*func TestSearchMsgSpecsNoResults(t *testing.T) {
-	req, err := http.NewRequest("GET", "", nil)
-	assert.Nil(t, err)
-	w := httptest.NewRecorder()
-	handler := http.HandlerFunc(SpecSearch)
-	handler.ServeHTTP(w, req)
-	assert.Equal(t, http.StatusOK, w.Code)
-}*/
+const (
+	searchTermAllResults = ""
+	searchTermNoResults  = "ujsdfusdfipsfipsfjöksfösfipsf"
+)
+
+func TestSearchMsgSpecsNoResults(t *testing.T) {
+	results := searchMsgSpecs(searchTermNoResults)
+	assert.Equal(t, 0, len(results))
+}
+
+func TestSearchMsgSpecsAllResults(t *testing.T) {
+	results := searchMsgSpecs(searchTermAllResults)
+	assert.Equal(t, 194, len(results))
+}
+
+func TestSearchSegSpecsNoResults(t *testing.T) {
+	results := searchSegSpecs(searchTermNoResults)
+	assert.Equal(t, 0, len(results))
+}
+
+func TestSearchSegSpecsAllResults(t *testing.T) {
+	results := searchSegSpecs(searchTermAllResults)
+	assert.Equal(t, 156, len(results))
+}
+
+func TestSearchCompositeDataElemSpecsNoResults(t *testing.T) {
+	results := searchCompositeDataElemSpecs(searchTermNoResults)
+	assert.Equal(t, 0, len(results))
+}
+
+func TestSearchCompositeDataElemSpecsAllResults(t *testing.T) {
+	results := searchCompositeDataElemSpecs(searchTermAllResults)
+	assert.Equal(t, 198, len(results))
+}
+
+func TestSearchSimpleDataElemSpecsNoResults(t *testing.T) {
+	results := searchSimpleDataElemSpecs(searchTermNoResults)
+	assert.Equal(t, 0, len(results))
+}
+
+func TestSearchSimpleDataElemSpecsAllResults(t *testing.T) {
+	results := searchSimpleDataElemSpecs(searchTermAllResults)
+	assert.Equal(t, 649, len(results))
+}
 
 func TestSpecSearchAllResults(t *testing.T) {
 	req, err := http.NewRequest("GET", "", nil)
@@ -41,7 +77,7 @@ func BenchmarkSpecSearchAllResults(b *testing.B) {
 }
 
 func BenchmarkSpecSearchNoResults(b *testing.B) {
-	req, err := http.NewRequest("GET", "?searchterm=sdfusfuspdfsdfjsfjsldfjopsfoa", nil)
+	req, err := http.NewRequest("GET", "?searchterm="+searchTermNoResults, nil)
 	assert.Nil(b, err)
 
 	b.ResetTimer()
